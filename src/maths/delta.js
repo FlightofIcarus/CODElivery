f9cd28d2ebe876619277b0ce3ffafc154bdf67f5
Tomás Leones <109169966+tleones@users.noreply.github.com>
2023-10-18 00:02:13 -0300

    Update delta.js
    Otimizando as linhas para deixar apenas o cálculo, sem exemplos.

--- src/maths/delta.js
+++ src/maths/delta.js
@@ -2,11 +2,4 @@ function calcularDelta(a, b, c) {
   return Math.pow(b, 2) - 4 * a * c;
 }
 
-// Exemplo de uso:
-const a = 5;
-const b = 9;
-const c = 2;
-const delta = calcularDelta(a, b, c);
-console.log("O valor de delta é: " + delta);
-
 module.exports = delta;
