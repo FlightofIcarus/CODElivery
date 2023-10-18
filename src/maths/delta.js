c8a5b9cb64226b8e5d7a57a7b135fe279248e8a9
Tomás Leones <109169966+tleones@users.noreply.github.com>
2023-10-17 23:44:13 -0300

    Create delta.js

---
+++ src/maths/delta.js
@@ -0,0 +1,10 @@
+function calcularDelta(a, b, c) {
+  return Math.pow(b, 2) - 4 * a * c;
+}
+
+// Exemplo de uso:
+const a = 5;
+const b = 9;
+const c = 2;
+const delta = calcularDelta(a, b, c);
+console.log("O valor de delta é: " + delta);
