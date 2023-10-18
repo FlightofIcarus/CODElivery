6b899d84731f047e450edbc686cc8a825ccd16b4
paulobof <paulobof@gmail.com>
2023-10-18 16:38:01 -0300

    CODElivery#34 - Criação da Função de Raiz Enesima

---
+++ src/maths/nthRoot.js
@@ -0,0 +1,25 @@
+/**
+ * @example
+ * const j = 27;
+ * const n = 3;
+ * const resultado = nthRoot(j, n);
+ * console.log(`A raiz ${n}-ésima de ${j} é: ${resultado}`);
+ */
+
+function nthRoot(j, n) {
+    if (n === 0) {
+        throw new Error("O valor de n não pode ser zero.");
+    }
+    
+    if (j < 0 && n % 2 === 0) {
+        throw new Error("Não é possível calcular raízes pares de números negativos.");
+    }
+
+    if (j < 0) {
+        return -Math.pow(-j, 1/n);
+    } else {
+        return Math.pow(j, 1/n);
+    }
+}
+
+module.exports = nthRoot;
